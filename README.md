a9293382743d119720243d9067a33c8a14ab4721
aindriu80 <aindriu80@gmail.com>
2024-02-12 13:50:50 +0000

    Project Setup Files

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-o learn React, check out the [React documentation](https://reactjs.org/).
 # academind-react-typescript-101
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <h3>academind-react-typescript-101</h3>
+    </div>
+  );
+}
+
+export default App;
