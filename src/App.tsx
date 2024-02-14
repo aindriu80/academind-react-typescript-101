3963c4b89832b314afdd14a3063538f9ab456076
aindriu80 <aindriu80@gmail.com>
2024-02-14 12:47:24 +0000

    Getting User Input with 'refs'

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import TodoList from "./components/TodoList";
+import NewTodo from "./components/NewTodo";
 
 const App: React.FC = () => {
   const todos = [{ id: "t1", text: "Finish the couse" }];
   return (
     <div className="App">
-      {/* A component that adds todos */}
       <h3>academind-react-typescript-101</h3>
+      <NewTodo />
       <TodoList items={todos} />
     </div>
   );
