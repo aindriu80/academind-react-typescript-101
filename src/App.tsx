4a5ef4462a871e3172dbc6c850142e6bd24f0f81
aindriu80 <aindriu80@gmail.com>
2024-02-15 11:28:19 +0000

    Cross Component Communication

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,15 @@ import NewTodo from "./components/NewTodo";
 
 const App: React.FC = () => {
   const todos = [{ id: "t1", text: "Finish the couse" }];
+
+  const todoAddHandler = (text: string) => {
+    console.log(text);
+  };
+
   return (
     <div className="App">
       <h3>academind-react-typescript-101</h3>
-      <NewTodo />
+      <NewTodo onAddTodo={todoAddHandler} />
       <TodoList items={todos} />
     </div>
   );
